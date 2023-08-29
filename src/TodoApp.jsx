a8547e9183c78c6ddd3742d04477b694047ef590
sid2261 <siddhartha2251@gmail.com>
2023-08-29 23:28:53 +0530

    1st commit

---
+++ src/TodoApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TodoApp() {
+  return (
+    <div>TodoApp</div>
+  )
+  let nextId = 4;
+  const initialTodos = [
+    { id: 1, todo: "Learn Javascript", done: true},
+    { id: 2, todo: "Build Spotify Clone", done: false},
+    { id: 3, todo: "Learn React", done: false},
+  ];
+}
+
+export default TodoApp
+
