cbcf500f371d7b33b926071cffd5386333c1a5dc
sid2261 <siddhartha2251@gmail.com>
2023-08-30 23:08:11 +0530

    commit

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useState } from 'react';
+import AddTodo from './AddTodo';
 
 function TodoApp() {
+    const [todos, setTodos] = useState(initialTodos);
+    function handleAddTodo(text) {
+        setTodos([
+            ...todos,
+            {
+                id: nextId++,
+                todo: text,
+                done: false,
+            },
+        ]); 
+    }
   return (
-    <div>TodoApp</div>
+    <>
+    <h2>TodoApp</h2>
+    <AddTodo onAddTodo={handleAddTodo}/>
+    </>
   )
   let nextId = 4;
   const initialTodos = [
